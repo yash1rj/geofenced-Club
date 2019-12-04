f66b1f1f872ef30c2556e34fcc239d2991b3aec0
yash1rj <yashrj130@gmail.com>
2019-12-04 17:14:57 +0530

    home component ts file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,19 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   
   username = 'John Doe';
+
+  pingServer(location) {
+    console.log("POST(send): ",location);
+    this.http
+      .post('http://localhost:4000/ping', location)
+      .subscribe((res) => {});
+  }
 
   ngOnInit() {
     if('geolocation' in navigator) {
       navigator.geolocation.watchPosition((pos) => {
-        console.log({
+        this.pingServer({
           lat: pos.coords.latitude,
           lng: pos.coords.longitude,
         });
-      })
+      });
     }
   }
-
 }
