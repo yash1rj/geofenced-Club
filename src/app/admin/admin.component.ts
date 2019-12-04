0cb6e1246e915625ac28a43f426e5b720f955009
yash1rj <yashrj130@gmail.com>
2019-12-04 17:14:45 +0530

    admin comp ts file

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
+import { PusherService } from '../pusher.service';
 
 declare const google;
 
@@ -10,18 +11,17 @@ declare const google;
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private loader: MapsAPILoader) { }
+  constructor(private loader: MapsAPILoader, private pusher: PusherService) { }
   
   theClubPolygon;
   username = 'Admin';
-  message = '';
   showAlert = false;
   showLocationUpdate = false;
   zoom = 15;
   // Center of the Club, where the initial marker will be placed
   center = {
     lat: 6.435838,
-    lng: 3.451384,
+    lng: 3.451384
   };
   // This array of latitude-longitudes represents the polygon around our ranch
   polygon = [
@@ -36,8 +36,31 @@ export class AdminComponent implements OnInit {
   ];
 
   ngOnInit() {
+    // Wait for the google maps script to be loaded before using the "google" keyword
     this.loader.load().then(() => {
       this.theClubPolygon = new google.maps.Polygon({paths: this.polygon});
+    });
+
+    // initialize Pusher
+    const channel = this.pusher.init();
+    
+    // listen for the ping event.
+    // In the bind callback, we set the center property to the position sent through the event.
+    channel.bind('ping', (pos) => {
+      this.center = {
+        ...pos,
+      };
+
+      // Create a LatLng using the position returned from the pusher event
+      const latLng = new google.maps.LatLng(pos);
+
+      this.showLocationUpdate = true;
+      
+      // Check if the location is outside the Club
+      if (!google.maps.geometry.poly.containsLocation(latLng, this.theClubPolygon)) {
+        // Show alert if user has left the polygon
+        this.showAlert = true;
+      }
     });
   }
 
